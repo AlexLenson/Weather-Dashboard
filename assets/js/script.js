460ce8e07dfa48397a6ff1c8f8335292088fd68e
Alex Lenson <144947695+AlexLenson@users.noreply.github.com>
2023-10-09 18:31:19 -0400

    added function to display search history to page and push unique cities to searchHistory array

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+$(function () {
+
+    // Add timezone plugins to day.js
+    dayjs.extend(window.dayjs_plugin_utc);
+    dayjs.extend(window.dayjs_plugin_timezone);
+
+    // global variables
+    var searchHistory = [];
+
+    // API endpoints
+    var geocodeUrl = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}";
+    var currentWeatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}";
+    var forecast5Url = "api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}";
+    var apiKey = "93c5997f1683b54331b6cb9a90e8dbca";
+
+    // DOM Elements
+    var cityForm = $("#city-form");
+    var cityInput = $("#city-input");
+    var submitBtn = $("#submit-btn");
+    var cityList = $("#city-list");
+
+    // Display search history to page and push to searchHistory array
+    function displayCity(event) {
+        event.preventDefault();
+        var inputText = cityInput.val().trim();
+        console.log(inputText);
+        if (inputText != "" && searchHistory.indexOf(inputText) === -1) {
+            // Creates new button
+            var button = $("<button>", {
+                type: "button",
+                class: "btn btn-secondary w-100 mt-3",
+                id: inputText,
+                text: inputText
+            });
+            cityList.prepend(button);
+            searchHistory.push(inputText);
+        }
+        cityInput.val("");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Event listeners
+    cityForm.on("submit", displayCity);
+
+
+
+
+});
